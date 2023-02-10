225fa860c5f3bfbbfef631e76819182cefae3bec
developmentHF <development.henry@gmail.com>
2023-02-10 22:55:40 +1100

    random letters generator added

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // Number of letters for players to play with
 const NUMLETTERS = 8 
-const lettersElem = document.querySelector(".letters");
+const validchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let pickedchars = [];
 
+// Random letters that words must be made up of
+const generateLetters = function() {
+    for (let i=0; i<NUMLETTERS; i++) {
+        let rngletter = validchars.charAt(Math.floor(Math.random() * 26));
+        console.log(rngletter)
+        pickedchars.push(rngletter);
+    }
+}
+
+// 
+
+generateLetters();
+
+const lettersElem = document.querySelector(".letters");
 for (let i=0; i<NUMLETTERS; i++) {
     const newLetter = document.createElement("div");
     newLetter.classList.add("letter");
-    newLetter.style.flex = 1;
-    newLetter.textContent = 'X';
+    newLetter.textContent = pickedchars[i];
     lettersElem.appendChild(newLetter);
 }
